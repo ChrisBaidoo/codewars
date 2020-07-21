ef0e8905c2cc1eb9cb3a03a145093037b1967ac1
christian baidoo <chris_pens@hotmail.co.uk>
2020-07-21 09:49:19 +0100

    Solved

--- codeWars_stringEndsWith.js
+++ codeWars_stringEndsWith.js
@@ -1,10 +1,13 @@
 //Complete the solution so that it returns true if the first argument(string)
 //passed in ends with the 2nd argument (also a string).
 
+//The first string should end with the second (whole) string
+
 function solution(str, ending) {
-  console.log(str);
-  console.log(ending.length);
-  return str.charAt(str.length - 1) === ending.charAt(ending.length - 1)
-    ? true
-    : false;
+  return str.includes(ending, str.length - ending.length);
+}
+
+// After submitting my answer and looking at other solutions, here is an altenative
+function solution(str, ending) {
+  return str.endsWith(ending);
 }
