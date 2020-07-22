6567fce8437a58c007cb81840762bd7bb7bee0d7
christian baidoo <chris_pens@hotmail.co.uk>
2020-07-22 23:49:10 +0100

    started find the stry numbers chaallenge

---
+++ codeWars_findTheStrayNumbers.js
@@ -0,0 +1,39 @@
+function stray(numbers) {
+  //sort the array
+  let sorted = numbers.sort((a, b) => {
+    a - b;
+  });
+
+  //push odd one into an array
+  let odd = [];
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted(i) !== sorted(i + 1)) {
+      odd.push(i);
+    }
+  }
+  //     sorted.map((num)=>{
+  //       if(num !== sorted[num+1]){
+  //         odd.push(num)
+  //       }
+  //     })
+  console.log(sorted);
+  console.log(odd);
+}
+
+function stray(numbers) {
+  //sort the array
+  let sorted = numbers.sort((a, b) => {
+    a - b;
+  });
+  let odd = [];
+
+  let max = Math.max(...sorted);
+  let min = Math.min(...sorted);
+
+  sorted.map((num) => {
+    if (num == max) {
+      console.log(max);
+    }
+    console.log(min);
+  });
+}
