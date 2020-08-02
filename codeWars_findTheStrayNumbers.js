5c5ea68d0dc5bd71f14b1db6ca7e90bffea1ae08
christian baidoo <chris_pens@hotmail.co.uk>
2020-08-02 21:54:29 +0100

    find the sequence kata done

--- codeWars_findTheStrayNumbers.js
+++ codeWars_findTheStrayNumbers.js
@@ -1,3 +1,10 @@
+//You are given an odd-length array of integers, in which
+//all of them are the same, except for one single number.
+
+//Complete the method which accepts such an array, 
+//and returns that single different number.
+
+
 function stray(numbers) {
   let groupA = [];
   let groupB = [];
---
+++ codeWars_sequence.js
@@ -0,0 +1,19 @@
+//Complete the solution so that it returns
+//the greatest sequence of five consecutive digits
+//found within the number given. The number will be
+//passed in as a string of only digits. It should
+//return a five digit integer. The number passed
+//may be as large as 1000 digits.
+
+function solution(digits) {
+  let newArray = [];
+  for (let i = 0; i < digits.length - 4; i++) {
+    newArray.push(digits.slice(i, 5 + i));
+  }
+
+  let highestNumber = Math.max(...newArray);
+
+  console.log(highestNumber);
+
+  return highestNumber;
+}
