95de5d9a616a5f338d74903e7d7576491aa33d43
christian baidoo <chris_pens@hotmail.co.uk>
2020-08-17 20:50:50 +0100

    Disemvowell trolls kata done

--- descendingOrder.js
+++ descendingOrder.js
@@ -1,3 +1,8 @@
+//Your task is to make a function that can take any
+//non-negative integer as a argument and return it
+//with its digits in descending order. Essentially,
+//rearrange the digits to create the highest possible number.
+
 function descendingOrder(n) {
   let num = String(n).split("");
   return Number(num.sort((a, b) => b - a).join(""));
--- squareEveryNumber.js
+++ squareEveryNumber.js
@@ -1,3 +1,6 @@
+//Welcome. In this kata, you are asked to square every
+//digit of a number.
+
 function squareDigits(num) {
   let numbers = String(num).split("");
   let newNumbers = numbers.map((number) => number * number).join("");
