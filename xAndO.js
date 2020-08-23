27a9fc6cf265c665903de73ecca3cc1c25ddf711
christian baidoo <chris_pens@hotmail.co.uk>
2020-08-23 23:16:18 +0100

    X and O's kata solved

---
+++ xAndO.js
@@ -0,0 +1,18 @@
+//Check to see if a string has the same amount of 'x's and 'o's.
+//The method must return a boolean and be case insensitive.
+//The string can contain any char.
+
+function XO(str) {
+  let x = [];
+  let o = [];
+  let upperCase = str.toUpperCase();
+
+  for (let i = 0; i <= upperCase.length; i++) {
+    if (upperCase[i] == "X") {
+      x.push(upperCase[i]);
+    } else if (upperCase[i] == "O") {
+      o.push(upperCase[i]);
+    }
+  }
+  return x.length === o.length;
+}
