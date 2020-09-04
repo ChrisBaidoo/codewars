3ca570c02a68097ed037ce17fe94e1bf3f6dce98
christian baidoo <chris_pens@hotmail.co.uk>
2020-09-04 22:08:11 +0100

    sum of odd numbers kata solved

---
+++ sumOfOddNumbers.js
@@ -0,0 +1,6 @@
+//Given the triangle of consecutive odd numbers:
+//Calculate the row sums of this triangle from the row index (starting at index 1)
+
+function rowSumOddNumbers(n) {
+  return Math.pow(n, 3);
+}
