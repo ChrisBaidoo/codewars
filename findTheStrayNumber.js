ae2ee3af9edcf80d099a3681a5123880f55e83eb
christian baidoo <chris_pens@hotmail.co.uk>
2020-09-29 22:34:15 +0100

    Find the stray number kata solved

---
+++ findTheStrayNumber.js
@@ -0,0 +1,24 @@
+//You are given an odd-length array of integers, in which all of them are the same,
+//except for one single number.
+
+//Complete the method which accepts such an array, and returns that single different number.
+
+//The input array will always be valid! (odd-length >= 3)
+
+function stray(numbers) {
+  let groupA = [];
+  let groupB = [];
+
+  let max = Math.max(...numbers);
+
+  numbers.map((num) => {
+    if (num == max) {
+      return groupA.push(num);
+    }
+    return groupB.push(num);
+  });
+
+  if (groupA.length < groupB.length) {
+    return groupA[0];
+  } else return groupB[0];
+}
