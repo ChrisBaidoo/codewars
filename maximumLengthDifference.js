4a38ffcfde6081f7e2b8c784af52dfd8eebfa9fb
christian baidoo <chris_pens@hotmail.co.uk>
2020-10-10 20:47:50 +0100

    first commit

---
+++ maximumLengthDifference.js
@@ -0,0 +1,15 @@
+function mxdiflg(a1, a2) {
+  
+    let max = a1.map((item)=>{
+      return item.length
+    })
+    console.log(max)
+    console.log(Math.max(...max))
+      
+      let min = a2.map((item)=>{
+      return item.length
+    })
+    console.log(Math.min(...min))
+      
+        return max-min
+    }
