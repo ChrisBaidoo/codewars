b1d35d972d473bb366faf78177dbc9f832a954d6
christian baidoo <chris_pens@hotmail.co.uk>
2020-10-15 21:49:32 +0100

    refactored

--- sumMultiplesOfThreeAndFive.js
+++ sumMultiplesOfThreeAndFive.js
@@ -6,8 +6,9 @@
 
 function findSum(n) {
   let result = 0;
-  for (let i = 0; i <= n; i += 1) {
-    if (i % 3 === 0 || i % 5 === 0) result += i;
+  for (let i = 0; i <= n; i ++) {
+    if (i % 3 === 0 || i % 5 === 0) 
+    result += i;
   }
   return result;
 }
