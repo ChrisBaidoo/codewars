18620027698c5e9d557f39a629cac14bb2fd7345
Christian Baidoo <christianbaidoo@Christians-MacBook-Pro.local>
2020-10-31 20:00:41 +0000

    first commit

---
+++ couponsCode.js
@@ -0,0 +1,23 @@
+//Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering 
+//invalid codes or using expired coupons.
+
+//Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+
+//A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in 
+//this format: "MONTH DATE, YEAR".
+
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+    console.log(currentDate)
+      console.log( expirationDate)
+  if (enteredCode == correctCode && currentDate == expirationDate){
+    console.log('yes')
+  }
+   
+    let time = new Date()
+    
+    var n = time.getMonth()
+    
+    console.log(n + 1)
+    
+  }
