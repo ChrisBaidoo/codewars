79b35943ad7abe909a54f904d4fdfcab6d5129a7
christian baidoo <chris_pens@hotmail.co.uk>
2020-11-06 19:39:56 +0000

    kata solved
     Author:    christian baidoo <chris_pens@hotmail.co.uk>

--- countTheDivisors.js
+++ countTheDivisors.js
@@ -7,6 +7,14 @@
 // divisors(12) = 6  // 1, 2, 3, 4, 6, 12
 // divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
 
+
 function getDivisorsCnt(n){
-    // todo
+    let divisors = []
+
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            divisors.push(i)
+        }
+    }
+  return divisors.length
 }
