2a1e966016c30398b5526f4ca69de8a2c5f143e7
Christian Baidoo <chris_pens@hotmail.co.uk>
2020-12-07 23:48:23 +0000

    first commit

---
+++ findTheMinimumSum.js
@@ -0,0 +1,16 @@
+//Given a 2D array of size m * n. Your task is to find the sum of minimum value in each row.
+
+/*For Example: [
+  [1, 2, 3, 4, 5],       // minimum value of row is 1
+  [5, 6, 7, 8, 9],       // minimum value of row is 5
+  [20, 21, 34, 56, 100]  // minimum value of row is 20
+]*/
+
+//So, the function should return 26 because sum of minimums is as 1 + 5 + 20 = 26
+//You will be always given non-empty array containing Positive values.
+
+
+function sumOfMinimums(arr) {
+    let array = arr.map((item)=>{return item.map((inside)=>inside*2)})
+    console.log(array)
+  }
