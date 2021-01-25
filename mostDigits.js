b3f52c36c1b4e902d7ddc2234a50e84cdc481bb4
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-01-25 23:01:45 +0000

    kata solved

--- mostDigits.js
+++ mostDigits.js
@@ -2,37 +2,13 @@
 //If two numbers in the argument array have the same number of digits, return the first one in the array.
 
 function findLongest(array){
+  let newArr = array.map(function(element) {
+    return element.toString()
+  })
   
-    //   let mapped = array.map((item)=>{
-    //     return item.toString().length
-    //   })
-      
-      console.log(Math.max(...array))
-      
-      let arrayLength = []
-      
-      
-      for(let i = 0; i < array.length; i++){
+  newArr.sort(function(a,b) {
+    return b.length - a.length
+  })
     
-        arrayLength.push(String(array[i]).length)
-        
-      }
-        
-    
-          console.log(arrayLength)
-    
-            for(let i = 0; i < arrayLength.length; i++){
-            if (String(arrayLength[i]) === String(arrayLength[i + 1])){
-          console.log("hello")
-        }
-      }
-      
-            console.log(arrayLength)
-      
-      //We need to sort  arrayLength from minimum to maximum then do a comparison if two numbers are the same 
-    
-      
-      
-    
-      
-    }
+  return parseInt(newArr[0])  
+}
