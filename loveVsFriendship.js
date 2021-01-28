d298a7ff1454be99320c6ed104f1ef29a85fe6d4
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-01-28 23:08:49 +0000

    Kata solved

--- loveVsFriendship.js
+++ loveVsFriendship.js
@@ -1,3 +1,8 @@
 function wordsToMarks(string){
-    console.log(string)
-    }
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let marks = 0;
+    string.split('').forEach((char) => {
+      marks += alphabet.indexOf(char) + 1;
+    });
+    return marks;
+  }
