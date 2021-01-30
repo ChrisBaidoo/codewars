ed564499c1af0616a95860c0ae453490150a3218
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-01-30 01:40:37 +0000

    kata solved

---
+++ mergeTwoArrays.js
@@ -0,0 +1,9 @@
+function mergeArrays(a, b) {
+    let mergedArrays = [],
+        longest = a.length > b.length ? a:b;
+    for (let i=0; i<longest.length; i++){
+      if (a[i]) j.push(a[i])
+      if (b[i]) j.push(b[i])
+    }
+    return mergedArrays
+  }
