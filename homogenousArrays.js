a14d76132633263193581e681e98a5914a746a30
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-02-02 20:48:23 +0000

    kata solved

--- homogenousArrays.js
+++ homogenousArrays.js
@@ -1,19 +1,15 @@
 function filterHomogenous(arrays) {
-    console.log(arrays)
+  
+    let newArray = []
+    
+    arrays.map((arra)=>{ 
       
-      let newArray = []
-      
-      arrays.map((arra)=>{ 
-        
-        if (new Set( arra.map( item => typeof item ) ).size <= 1){
-          
-    newArray.push(arra)
-        }
-        
-        })
-      
-      console.log(newArray.filter((item)=>{
-        return item.length === 0
-      }))
-                  
-    }
+      if (new Set( arra.map( item => typeof item ) ).size <= 1){
+        newArray.push(arra)
+      }
+       })
+      return newArray.filter((item)=>{
+      return item.length !== 0
+    })
+                
+  }
