398b9313c71d69b052bef48fe2f5e9836fdb71e2
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-02-05 23:50:26 +0000

    add comments

--- missingValues.js
+++ missingValues.js
@@ -1,3 +1,7 @@
+//You are given a sequence of positive ints where every element appears three times, except one that 
+//appears only once (let's call it x) and one that appears only twice (let's call it y).
+
+
 function missingValues(a) {
     const x = a.find( v => a.filter( w => v===w ).length===1 );
     const y = a.find( v => a.filter( w => v===w ).length===2 );
