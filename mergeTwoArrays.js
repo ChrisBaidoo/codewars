a074618a5a5abdc0bc485f44d3544fcce3169f26
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-02-10 22:21:00 +0000

    added comment

--- mergeTwoArrays.js
+++ mergeTwoArrays.js
@@ -1,3 +1,7 @@
+//Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+//[a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+//[1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+
 function mergeArrays(a, b) {
     let mergedArrays = [],
         longest = a.length > b.length ? a:b;
