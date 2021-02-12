24975d62b5af6e8d8e5448f45692074ea8fa6e0f
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-02-12 23:53:59 +0000

    Added comment

--- sorted-yes-no-how.js
+++ sorted-yes-no-how.js
@@ -1,3 +1,9 @@
+//Complete the method which accepts an array of integers, and returns one of the following:
+//"yes, ascending" - if the numbers in the array are sorted in an ascending order
+//"yes, descending" - if the numbers in the array are sorted in a descending order
+//"no" - otherwise
+//You can assume the array will always be valid, and there will always be one correct answer.
+
 function isSortedAndHow(array) {
     let newArrayForAscending = Array.from(array)
     let newArrayForDescending = Array.from(array)
