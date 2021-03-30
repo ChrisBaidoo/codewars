e0a7395870718d2fe50a9c080075ae9934946a23
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-03-30 04:00:35 +0100

    Altenative soloution

--- welcome.js
+++ welcome.js
@@ -25,3 +25,33 @@ function greet(lang) {
   'swedish': 'Valkommen',
   'welsh': 'Croeso'
   };
+
+
+
+  // My version
+
+  function greet(language) {
+    let languages = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso'
+    }
+    if (languages.hasOwnProperty(`${language}`)){
+      return languages[language]
+    }
+    return 'Welcome'
+  }
