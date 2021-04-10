4bdded9a76e010f9bab5c60930d451ab04c73eac
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-04-10 14:26:17 +0100

    Is ruby developer coming kata solved

--- codingMeetUpSeries.js
+++ codingMeetUpSeries.js
@@ -28,3 +28,16 @@ function countDevelopers(list) {
     })
     return javascript.length
 }
+
+/* 
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have 
+signed up to attend the next coding meetup that you are organising.
+Your task is to return:
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+*/
+
+function isRubyComing(list) {
+  let rubyDev = list.find((dev)=>dev.language == "Ruby")
+  return rubyDev? true : false
+  }
