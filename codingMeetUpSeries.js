ba09970438bb18896092c10498d577491f331e77
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-04-21 04:08:09 +0100

    the oldest developer kata WIP

--- codingMeetUpSeries.js
+++ codingMeetUpSeries.js
@@ -80,3 +80,16 @@ Alternative solution
 function isSameLanguage(list) {
   return new Set(list.map((dev) => dev.language)).size == 1;
 }
+
+/* 
+Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, 
+include all same-age senior developers listed in the same order as they appeared in the original 
+input array.
+*/
+
+function findSenior(list) {
+  let sort = list.sort((a, b) => {
+    return a.age - b.age;
+  });
+  console.log(sort.length - 1);
+}
