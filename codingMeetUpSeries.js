07068e6658d91171b21b891a9968c7fd8544644e
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-08-10 23:13:51 +0100

    Sort an array alphabetically kata solved

--- codingMeetUpSeries.js
+++ codingMeetUpSeries.js
@@ -296,3 +296,18 @@ function findOddNames(list) {
     return ascii % 2 == 1;
   });
 }
+
+/* Write a function that returns the array sorted alphabetically by the programming language. In case there 
+are some developers that code in the same language, sort them alphabetically by the first name:*/
+
+function sortByLanguage(list) {
+  return list.sort((a, b) =>
+    a.language > b.language
+      ? 1
+      : a.language === b.language
+      ? a.firstName > b.firstName
+        ? 1
+        : -1
+      : -1
+  );
+}
