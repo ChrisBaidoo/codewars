678fcdbaf105cca05d3a0803d5a2bcc6311452df
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-09-20 22:35:03 +0100

    first commit

---
+++ buildingBlocks.js
@@ -0,0 +1,23 @@
+/*Write a class Block that creates a block (Duh..)
+##Requirements
+The constructor should take an array as an argument, this will contain 3 integers of the 
+form [width, length, height] from which the Block should be created.
+Define these methods: 
+
+`getWidth()` return the width of the `Block`
+
+`getLength()` return the length of the `Block`
+
+`getHeight()` return the height of the `Block`
+
+`getVolume()` return the volume of the `Block`
+
+`getSurfaceArea()` return the surface area of the `Block`
+
+*/
+
+class Block {
+  constructor(data) {
+    console.log(data);
+  }
+}
