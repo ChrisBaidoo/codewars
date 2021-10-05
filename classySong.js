83b0c6ad9a3691249eb1f5c70b3eaad677bc3d15
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-10-05 05:02:22 +0100

    classy Song WIP

--- classySong.js
+++ classySong.js
@@ -2,16 +2,17 @@ class Song {
   constructor(name, artist) {
     this.name = name;
     this.artist = artist;
+    this.listener = new Set();
   }
-  howMany(a) {
-    return a.length;
+  howMany(people) {
+    let listenerSize = this.listener.size;
+    let q = people.map((a) => {
+      return a.toLowerCase();
+    });
+    console.log(q);
+    this.listener.add(q.join(", "));
+    console.log(this.listener);
+
+    return this.listener.size - listenerSize;
   }
 }
-
-const check = new Song("hek", "hjgk");
-
-console.log(check);
-
-check.howMany(["a", "df", "sd"]);
-
-console.log(check);
