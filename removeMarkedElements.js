d3c04dfe0436a087207852a302c2082b39ac1ad8
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-10-20 23:15:47 +0200

    Kata solved

--- removeMarkedElements.js
+++ removeMarkedElements.js
@@ -1,10 +1,4 @@
 //Define a method/function that removes from a given array of integers all the values contained
 //in a second array.
 
-Array.prototype.remove_ = function (integer_list, values_list) {
-  console.log(
-    integer_list.filter((e) => {
-      values_list.map((f) => e != f);
-    })
-  );
-};
+Array.prototype.remove_ = (x, y) => x.filter((e) => !y.includes(e));
