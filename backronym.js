d787dc83525a2d6a800dc1fcaa9e24a95a536904
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-11-10 22:19:31 +0100

    refactored code

--- backronym.js
+++ backronym.js
@@ -1,3 +1,8 @@
+/*Complete the function to create backronyms. Transform the given string (without spaces) to a backronym, 
+using the preloaded dictionary and return a string of words, separated with a single space 
+(but no trailing spaces). The keys of the preloaded dictionary are uppercase letters A-Z and the values are 
+predetermined words, for example: */
+
 const makeBackronym = function (string) {
   const array = string.toUpperCase().split("");
 
@@ -12,4 +17,13 @@ const makeBackronym = function (string) {
     words.push(dict[array[i]]);
   }
   return `${words.join(" ")}`;
+};
+
+/* Altenative Solution */
+var makeBackronym = function (string) {
+  return string
+    .toUpperCase()
+    .split("")
+    .map((letter) => dict[letter])
+    .join(" ");
 };
