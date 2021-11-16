c55edfd61d0351607ce528a32e4f7389ee3f7a30
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-11-16 23:15:02 +0100

    altenative solution

--- chuckNorris.js
+++ chuckNorris.js
@@ -4,3 +4,10 @@ function onePunch(items) {
   }
   return items.split(" ").sort().join(" ").replace(/[ea]/gi, "");
 }
+
+/* Altenative Solution */
+
+const onePunch = (items) =>
+  typeof items !== "string" || items.length == 0
+    ? "Broken!"
+    : items.split(" ").sort().join(" ").replace(/[ea]/gi, "");
