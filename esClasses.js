a02d5da3d68b57bf60f2a41fd742e0ee5c05ea61
Christian Baidoo <chris_pens@hotmail.co.uk>
2021-11-30 05:06:16 +0000

    esClassesWIP

---
+++ esClasses.js
@@ -0,0 +1,33 @@
+/* 
+Define the following classes.
+
+I. Cuboid
+The object constructor for the class Cuboid should receive exactly three arguments in the following order: length, 
+width, height and store these three values in this.length, this.width and this.height respectively.
+The class Cuboid should then have a getter surfaceArea which returns the surface area of the cuboid and a getter 
+volume which returns the volume of the cuboid.
+
+II. Cube
+class Cube is a subclass of class Cuboid. The constructor function of Cube should receive one argument only, its 
+length, and use that value passed in to set this.length, this.width and this.height.
+*/
+
+class Cuboid {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  surfaceArea() {
+    let a = this.length * this.width * 2;
+    let b = this.height * this.width * 2;
+    let c = this.length * this.height * 2;
+    let d = a + b + c;
+    return d;
+  }
+  volume() {
+    return this.width * this.length * this.height;
+  }
+}
+
+class Cube extends Cuboid {}
