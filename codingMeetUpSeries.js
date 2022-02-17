0e652057edeacaaaf79428bee93600cec2c99830
Christian Baidoo <chris_pens@hotmail.co.uk>
2022-02-17 04:07:36 +0000

    altenative for solution for getFirstPython kata

--- codingMeetUpSeries.js
+++ codingMeetUpSeries.js
@@ -1,7 +1,14 @@
+// https://www.codewars.com/kata/58287977ef8d4451f90001a0
+
 /*
 You will be given an array of objects (associative arrays in PHP) representing data about developers who 
 have signed up to attend the next coding meetup that you are organising. The list is ordered according to 
 who signed up first.
+
+Your task is to return one of the following strings:
+
+< firstName here >, < country here > of the first Python developer who has signed up; or
+There will be no Python developers if no Python developer has signed up.
 */
 
 function getFirstPython(list) {
@@ -11,6 +18,15 @@ function getFirstPython(list) {
     }
   }
   return `There will be no Python developers`;
+}
+
+// Altenative Solution
+
+function getFirstPython(list) {
+  const dev = list.find((x) => x.language === "Python");
+  return dev
+    ? `${dev.firstName}, ${dev.country}`
+    : "There will be no Python developers";
 }
 
 /*
