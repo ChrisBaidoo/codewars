3dc7716e775a1b791dfec239671ffced0618ed48
Chris <chris_pens@hotmail.co.uk>
2022-08-25 23:27:08 +0100

    change commenting style

--- areTheNumbersInOrder.js
+++ areTheNumbersInOrder.js
@@ -4,15 +4,15 @@
 
 //For the purposes of this Kata, you may assume that all inputs are valid, i.e. non-empty arrays containing only integers.
 
-//Note that an array of 1 integer is automatically considered to be sorted in ascending order since all (zero) adjacent 
-//pairs of integers satisfy the condition that the left integer does not exceed the right integer in value. An empty list is 
-//considered a degenerate case and therefore will not be tested in this Kata - feel free to raise an Issue if you see such a 
-//list being tested.
+/* Note that an array of 1 integer is automatically considered to be sorted in ascending order since all (zero) adjacent 
+pairs of integers satisfy the condition that the left integer does not exceed the right integer in value. An empty list is 
+considered a degenerate case and therefore will not be tested in this Kata - feel free to raise an Issue if you see such a 
+list being tested.
 
-//For example:
+For example:
 
-//inAscOrder([1,2,3,4,5]); // returns true
-//inAscOrder([1,6,10,18,2,4,20]); // returns false
+inAscOrder([1,2,3,4,5]);  returns true
+inAscOrder([1,6,10,18,2,4,20]); returns false */
 
 
 function inAscOrder(arr) {
