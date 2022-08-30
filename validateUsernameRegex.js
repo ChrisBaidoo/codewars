7bd8c8806fc0fd028c0e93df1f9dc746aa8dc56b
Chris <chris_pens@hotmail.co.uk>
2022-08-30 23:35:09 +0100

    check length is between 4 and 16 characters

--- validateUsernameRegex.js
+++ validateUsernameRegex.js
@@ -5,10 +5,9 @@ underscore
 Length should be between 4 and 16 characters (both included).*/
 
 function validateUsr(username) {
-  const pattern = /^[A-Za-z0-9_]*$/
+  const pattern = /([a-z]){4,16}$/   
   console.log(username)
   console.log(pattern.test(username))
   return pattern.test(username)
-
 }
 
