c90e8f381244fc8d4fd7cf96925636ed61a3f2cb
Chris <chris_pens@hotmail.co.uk>
2023-01-18 05:23:14 +0000

    altenative solution

--- altenateCapitalisation.js
+++ altenateCapitalisation.js
@@ -2,25 +2,42 @@
 For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
 The input will be a lowercase string with no spaces. */
 
-function capitalize(s){
+function capitalize(s) {
+  let even = [];
+  let odd = [];
 
-  let even = []
-  let odd = []
-    
-  for(i = 0; i < s.length; i++){
-    if(i % 2 === 0){
-     even.push(s[i].toUpperCase())
-    }
-    else even.push(s[i])
+  for (i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      even.push(s[i].toUpperCase());
+    } else even.push(s[i]);
   }
-    
-    for(i = 0; i < s.length; i++){
-    if(i % 2 === 0){
-     odd.push(s[i])
-    }
-    else odd.push(s[i].toUpperCase())
+
+  for (i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      odd.push(s[i]);
+    } else odd.push(s[i].toUpperCase());
   }
-    
-    let together = [String(even.join('')), (String(odd.join('')))]
-    return together
-  };
+
+  let together = [String(even.join("")), String(odd.join(""))];
+  return together;
+}
+
+//Altenative solution
+
+function capitalize(s) {
+  let even = [];
+  let odd = [];
+
+  for (i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      even.push(s[i].toUpperCase());
+      odd.push(s[i]);
+    } else {
+      even.push(s[i]);
+      odd.push(s[i].toUpperCase());
+    }
+  }
+
+  let together = [String(even.join("")), String(odd.join(""))];
+  return together;
+}
