9a51c2d2e62a8afdd9c4e6450ab89bd9466e5aae
Chris <chris_pens@hotmail.co.uk>
2023-01-20 04:18:32 +0000

    Altenative solution

--- xAndO.js
+++ xAndO.js
@@ -16,3 +16,11 @@ function XO(str) {
   }
   return x.length === o.length;
 }
+
+//Altenative solution
+function XO(str) {
+  let x = str.match(/x/gi);
+  let o = str.match(/o/gi);
+
+  return (x && x.length) === (o && o.length);
+}
