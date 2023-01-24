4a98d1498aa464bd9e58e8cb9c5ca45d2b343273
Chris <chris_pens@hotmail.co.uk>
2023-01-24 23:36:30 +0000

    Altenative solution

--- countTheDivisors.js
+++ countTheDivisors.js
@@ -7,14 +7,20 @@
 // divisors(12) = 6  // 1, 2, 3, 4, 6, 12
 // divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
 
+function getDivisorsCnt(n) {
+  let divisors = [];
 
-function getDivisorsCnt(n){
-    let divisors = []
-
-    for(let i = 1; i <= n; i++){
-        if(n % i === 0){
-            divisors.push(i)
-        }
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      divisors.push(i);
     }
-  return divisors.length
+  }
+  return divisors.length;
 }
+
+//Altenative solution
+const getDivisorsCnt = (n) => {
+  let count = 0;
+  for (let i = 1; i <= n; i++) if (n % i == 0) count++;
+  return count;
+};
