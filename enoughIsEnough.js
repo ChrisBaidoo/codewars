1e1f1126c3e62d836a17d7b2dca634384966894d
Chris <chris_pens@hotmail.co.uk>
2023-02-07 22:47:43 +0000

    kata solved

--- enoughIsEnough.js
+++ enoughIsEnough.js
@@ -14,3 +14,11 @@ drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times
 leads to [1,2,3,1,2,3].
 With list [20,37,20,21] and number 1, the result would be [20,37,21]
 */
+
+function deleteNth(arr, x) {
+  let obj = {};
+  return arr.filter(function (number) {
+    obj[number] = obj[number] ? obj[number] + 1 : 1;
+    return obj[number] <= x;
+  });
+}
