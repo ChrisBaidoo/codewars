5432998f9a5ca5823897d9ae73dbac1306ba74dc
Chris <chris_pens@hotmail.co.uk>
2023-02-10 00:03:04 +0000

    Altenative solution

--- StopgninnipSMysdroW.js
+++ StopgninnipSMysdroW.js
@@ -11,3 +11,13 @@ function spinWords(string) {
   });
   return String(reverse).replace(/,/g, " ");
 }
+
+//Altenative solution
+function spinWords(words) {
+  return words
+    .split(" ")
+    .map(function (word) {
+      return word.length > 4 ? word.split("").reverse().join("") : word;
+    })
+    .join(" ");
+}
