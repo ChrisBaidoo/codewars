9cb39d193dbf5992aac4630a68aed74ef9b0886c
Chris <chris_pens@hotmail.co.uk>
2023-02-27 23:15:59 +0000

    altenative solution

--- parityOutlier.js
+++ parityOutlier.js
@@ -27,3 +27,11 @@ function findOutlier(integers) {
   }
   return even[0];
 }
+
+//Altenative solution
+
+function findOutlier(int) {
+  const even = int.filter((a) => a % 2 == 0);
+  const odd = int.filter((a) => a % 2 !== 0);
+  return even.length == 1 ? even[0] : odd[0];
+}
