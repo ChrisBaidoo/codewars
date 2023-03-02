d81016bd546636ad091728f8ef83fe85e990df17
Chris <chris_pens@hotmail.co.uk>
2023-03-02 23:45:29 +0000

    kata solved

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -11,7 +11,12 @@ Examples
 
 */
 
-function duplicateEncode(word){
-const lowerCase = word.toLowerCase()
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
 }
-
