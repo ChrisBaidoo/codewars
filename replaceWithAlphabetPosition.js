5abde0fecab8caf283bf4be4bb7cbf8dddc17a7c
Chris <chris_pens@hotmail.co.uk>
2023-03-15 20:43:06 +0000

    altenative solution

--- replaceWithAlphabetPosition.js
+++ replaceWithAlphabetPosition.js
@@ -39,3 +39,18 @@ function alphabetPosition(text) {
   });
   return lettersIndex.join(" ");
 }
+
+//Altenative solutions
+
+function alphabetPosition(text) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  return text
+    .toLowerCase()
+    .split("")
+    .filter((letter) => {
+      let index = alphabet.indexOf(letter);
+      return index > -1;
+    })
+    .map((letter) => alphabet.indexOf(letter) + 1)
+    .join(" ");
+}
