8b675cf4a87239714d5039640dd3ce949a9c5338
Chris <chris_pens@hotmail.co.uk>
2023-03-27 09:33:27 +0100

    kata solved

--- uniqueInOrder.js
+++ uniqueInOrder.js
@@ -7,8 +7,11 @@ uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
 uniqueInOrder([1,2,2,3,3])       == [1,2,3]
 */
 
-var uniqueInOrder = function (iterable) {
+const uniqueInOrder = function (iterable) {
   //your code here - remember iterable can be a string or an array
-  const uniqueValues = [...new Set(iterable)];
+  const turnToArray = Array.isArray(iterable) ? iterable : iterable.split("");
+  const uniqueValues = turnToArray.filter((x, i) => {
+    return turnToArray[i] != turnToArray[i + 1];
+  });
   return uniqueValues;
 };
