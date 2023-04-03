4bda007ba8e9b4a6d6e0bcbd6d5ccbee40ab3cfa
Chris <chris_pens@hotmail.co.uk>
2023-04-03 14:07:07 +0100

    altenative solution

--- yourOrderPlease.js
+++ yourOrderPlease.js
@@ -27,3 +27,13 @@ function order(words) {
   });
   return keysSorted.join(" ");
 }
+
+// Altenative solution
+function order(words) {
+  return words
+    .split(" ")
+    .sort(function (a, b) {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(" ");
+}
