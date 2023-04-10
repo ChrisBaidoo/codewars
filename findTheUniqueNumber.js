d036f1c90c35099825656086f605bcf49e0fb6dc
Chris <chris_pens@hotmail.co.uk>
2023-04-10 23:55:50 +0100

    first commit

---
+++ findTheUniqueNumber.js
@@ -0,0 +1,12 @@
+/* 
+There is an array with some numbers. All numbers are equal except for one. Try to find it!
+It’s guaranteed that array contains at least 3 numbers.
+The tests contain some very huge arrays, so think about performance.
+This is the first kata in series:
+*/
+
+function findUniq(arr) {
+  let unique = arr.filter((letter, i) => {
+    return arr[i] != arr[i + 1];
+  });
+}
