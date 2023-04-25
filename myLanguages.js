33a2ed28416b2f5d910388c20d8aedd5c45e5bd2
Chris <chris_pens@hotmail.co.uk>
2023-04-25 22:38:42 +0100

    altenative solutions

--- myLanguages.js
+++ myLanguages.js
@@ -1,13 +1,12 @@
-//You are given a dictionary/hash/object containing some languages and your test results in the given 
-//languages. Return the list of languages where your test score is at least 60, in descending order of 
+//You are given a dictionary/hash/object containing some languages and your test results in the given
+//languages. Return the list of languages where your test score is at least 60, in descending order of
 //the results.
 
 //Note: the scores will always be unique (so no duplicate values)
 
-
 function myLanguages(results) {
-      
-    let testScore =  Object.keys(results).filter(language => results[language] > 59).sort((a,b) => results[b] - results[a]);
-    console.log(items)
-    return testScore    
+  let testScore = Object.keys(results)
+    .filter((language) => results[language] > 59)
+    .sort((a, b) => results[b] - results[a]);
+  return testScore;
 }
