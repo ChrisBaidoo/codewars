d27f229e1bc5ffb7bf479c114520663edb9192fa
Chris <chris_pens@hotmail.co.uk>
2023-11-24 03:49:59 +0000

    kata solved

---
+++ getNames.js
@@ -0,0 +1,11 @@
+/*
+The following code is not giving the expected results. Can you debug what the issue is?
+
+The following is an example of data that would be passed in to the function.
+*/
+
+function getNames(data) {
+  return data.map(function (item) {
+    return item.name;
+  });
+}
